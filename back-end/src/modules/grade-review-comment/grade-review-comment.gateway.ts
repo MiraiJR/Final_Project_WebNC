a41a7f59baa7de56865046eec8bacfcf10717849
hihivipzz <81902603+hihivipzz@users.noreply.github.com>
2023-12-26 10:06:13 +0700

    Guard for socket

--- back-end/src/modules/grade-review-comment/grade-review-comment.gateway.ts
+++ back-end/src/modules/grade-review-comment/grade-review-comment.gateway.ts
@@ -1,12 +1,17 @@
 import { WebSocketGateway, WebSocketServer,SubscribeMessage,MessageBody,ConnectedSocket } from "@nestjs/websockets";
 import { Server,Socket } from "socket.io";
 import { GradeReviewCommentResponse } from "./dto/res/GradeReviewCommentResp.dto";
+import { UseGuards } from "@nestjs/common";
+import { ReviewSocketGuard } from "src/shared/guards/ReviewSocketGuard";
+import { SocketAuthGuard } from "src/shared/guards/SocketAuthGuard";
 
 
 @WebSocketGateway()
 export class GradeReviewCommentGateWay{
     @WebSocketServer() server: Server;
 
+    @UseGuards(ReviewSocketGuard)
+    @UseGuards(SocketAuthGuard)
     @SubscribeMessage('joinRoom')
     handleJoinRoom(@MessageBody() reviewId: number, @ConnectedSocket() client: Socket): void{
         client.join(`review-${reviewId}`);
@@ -17,7 +22,6 @@ export class GradeReviewCommentGateWay{
         client.leave(`review-${reviewId}`);
     }
 
-    @SubscribeMessage('comment')
     handleComment( comment: GradeReviewCommentResponse): void {
         this.server.to(`review-${comment.reviewId}`).emit('newComment', comment);
     }
--- front-end/src/shared/libs/socket.ts
+++ front-end/src/shared/libs/socket.ts
@@ -1,8 +1,10 @@
 import { io } from 'socket.io-client';
+import JwtStorage from "../storages/JwtStorage";
 
 const BE_SOCKET_URL = import.meta.env.VITE_BACKEND_URL.replace("/api/v1", "")
 
 export const socketComment = io(BE_SOCKET_URL,{
     autoConnect: false,
     transports : ['websocket'],
+    query: {token: JwtStorage.getToken()?.accessToken}
 });
