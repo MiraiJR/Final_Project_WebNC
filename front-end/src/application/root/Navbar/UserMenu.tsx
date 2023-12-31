1a0a9fd1eb313512f8ade9547767afa5eaae7107
PC <truongvanhao159@gmail.com>
2023-12-31 09:54:28 +0700

    Fix bug logout

--- front-end/src/application/root/Navbar/UserMenu.tsx
+++ front-end/src/application/root/Navbar/UserMenu.tsx
@@ -13,6 +13,9 @@ import { useNavigate } from "react-router-dom";
 import JwtStorage from "@/shared/storages/JwtStorage";
 import AuthService from "@/shared/services/AuthService";
 import { toast } from "react-toastify";
+import UserService from "@/shared/services/UserService";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useGlobalState } from "@/shared/storages/GlobalStorage";
 
 interface UserMenuProps {
   fullname: string;
@@ -20,47 +23,60 @@ interface UserMenuProps {
 
 export default function UserMenu({ fullname }: UserMenuProps) {
   const navigate = useNavigate();
+  const { setIsLogin } = useGlobalState();
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
+  const { logout } = useAuth0();
   const handleClose = () => {
     setAnchorEl(null);
   };
 
   const handleLogout = async () => {
     try {
+      const { data } = await UserService.getMe();
       await AuthService.logout();
       JwtStorage.deleteToken();
+      setIsLogin(false);
+
+      if (data.isSocialLogin) {
+        logout({
+          logoutParams: { returnTo: window.location.origin },
+        });
+        return;
+      }
+
       navigate("/auth/sign-in");
     } catch (error: any) {
       toast.error(error.message);
     }
   };
 
-    const handleProfile = ()=>{
-      navigate('/profile/update');
-    }
+  const handleProfile = () => {
+    navigate("/profile/update");
+  };
 
-    return(
-        <>
-        <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
-            <Tooltip title="Account settings">
-            <IconButton
-                onClick={handleClick}
-                size="small"
-                sx={{ ml: 2 }}
-                aria-controls={open ? "account-menu" : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? "true" : undefined}
-            >
-                <Avatar sx={{ width: 32, height: 32 }}>{Helper.getFullNameIcon(fullname)}</Avatar>
-            </IconButton>
-            </Tooltip>
-        </Box>
-        <Menu
-
+  return (
+    <>
+      <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
+        <Tooltip title="Account settings">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? "account-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+          >
+            <Avatar sx={{ width: 32, height: 32 }}>
+              {Helper.getFullNameIcon(fullname)}
+            </Avatar>
+          </IconButton>
+        </Tooltip>
+      </Box>
+      <Menu
         anchorEl={anchorEl}
         id="account-menu"
         open={open}
